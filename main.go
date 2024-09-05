bc980d9cb8b6953734a9ee2a447e6db6b6437065
kake87 <kazievkanat1@gmail.com>
2024-09-05 12:02:41 +0500

    Initial commit for user registration service

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"./models"
+)
+
+var db *gorm.DB
+
+func initDatabase() {
+	dsn := "host=localhost user=postgres password=yourpassword dbname=yourdb port=5432 sslmode=disable"
+
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Could not connect to the database &v", err)
+	}
+
+	log.Println("✅Succesful connect")
+}
+
+func main() {
+	initDatabase()
+	db.AutoMigrate(&models.User{})
+}
+
+
+
+
